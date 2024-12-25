46428c90b64bc4cb4183e4a45a468979ebda654b
Michal Porteš <michalportes1@gmail.com>
2024-12-25 08:10:53 +0100

    Define a task that doesn't store a result

--- project/tasks.py
+++ project/tasks.py
@@ -4,3 +4,9 @@ from .celery import app
 @app.task
 def power_sum(collection, power):
     return sum(item ** power for item in collection)
+
+
+@app.task(ignore_result=True)
+def write_to_text_file(value, filename):
+    with open(filename, "a") as f:
+        print(value, file=f)
--- run_tasks.py
+++ run_tasks.py
@@ -1,10 +1,12 @@
-from project.tasks import power_sum
+from project.tasks import power_sum, write_to_text_file
 
 if __name__ == "__main__":
     data = list(range(1_000_000))
 
     square_sum = power_sum.signature((2,))
-    square_sum.apply_async((data,))
-
     cube_sum = power_sum.s(3)  # shortcut for `.signature((3,))`
-    cube_sum.delay(data)  # shortcut for `.apply_async((data,))`
+
+    write_to_results = write_to_text_file.s('results.txt')
+
+    (square_sum | write_to_results)(data)
+    (cube_sum | write_to_results)(data)
